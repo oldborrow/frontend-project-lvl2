76298696a613455c419df6afd7aeb80a29fcadfc
oldborrow <spevak.vlad@gmail.com>
2021-04-01 17:57:13 +0300

    step 3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
+import diff from '../difflogic.js'
 
-const { program, option, help } = require('commander');
+import { Command } from 'commander/esm.mjs';
+const program = new Command();
 program.version('0.0.1');
 
 program
   .option('-f, --format [type]', 'output format')
+  .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => diff(filepath1, filepath2));
+
 program.parse(process.argv);
-
-const options = program.opts();
-
